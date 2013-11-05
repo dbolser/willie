bdac24c72472c35dba2c285f6157a05d203d73f0
grabmuel <grabmuel@ebi.ac.uk>
2013-11-05 11:21:12 +0000

    might as well use Beautiful Soup if we're using virtualenv

--- willie/modules/grabmuel.py
+++ willie/modules/grabmuel.py
@@ -1,28 +1,35 @@
-import willie
-import urllib2
+from willie.module import commands, example
+from willie import web
 import xml.etree.ElementTree as ET
-import re
+from bs4 import BeautifulSoup
 
-@willie.module.commands('ena')
-def ena_lookup(bot, trigger):
+@commands('ena')
+def ena_lookup(bot, trigger):	
 	url = 'http://www.ebi.ac.uk/ena/data/view/%s&display=xml' % trigger.group(2)
-	page = urllib2.urlopen(url).read()
+	page = web.get(url)
 	tree = ET.fromstring(page)
-	desc = tree.iter('description').next().text
-	bot.say(desc)
+	if tree.text.strip().endswith('not found.'):
+		bot.say("ENA doesn't have an entry for '%s'" % trigger.group(2))
+	else:
+		try:
+			desc = tree.iter('DESCRIPTION').next().text
+			bot.say(desc)
+		except StopIteration:
+			bot.say("entry for '%s' doesn't have a DESCRIPTION tag" % trigger.group(2))
 
-@willie.module.commands('ebistaff')
-def ebi_people(bot, trigger):
+@commands('ebistaff')
+def ebi_staff_search(bot, trigger):
 	url = 'http://www.ebi.ac.uk/ebisearch/search.ebi?query=%s&db=ebiweb' % trigger.group(2)
-	page = urllib2.urlopen(url).read()
-	if 'View in the website' in page:
-		name = re.search('title="View in the website" rel="nofollow">(.+?)</a>', page, re.DOTALL).group(1)
-		name = ' '.join(name.split())	
-		name = unicode(name, 'utf-8').encode('ascii', 'replace')
-		room = re.search('Room: </span>(.+?)<br />', page).group(1)
-		telephone = re.search('Tel: </span>(.+?)<br />', page).group(1)
-		position = re.search('Position\(s\):.+?<span >(.+?)</span>', page, re.DOTALL).group(1)
-		position = ' '.join(position.split())
+	page = web.get(url)	
+	soup = BeautifulSoup(page)	
+	name_link = soup.find('a', title='View in the website')
+	if name_link:		
+		name = name_link.text
+		name = ' '.join(name.split())
+		room = soup.find('span', text='Room: ').next_sibling
+		telephone = soup.find('span', text='Tel: ').next_sibling
+		position = soup.find('span', text="Position(s): ").next_sibling.next.text
+		position = ' '.join(position.split())		
 		bot.say('%s | room %s | %s |  %s' % (name, room, telephone, position))
 	else:
 		bot.say("couldn't find '%s'" % trigger.group(2))
